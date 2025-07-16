37dc5e08dde1cbd9e4ba9775c2772af9e6003bd3
Tale <nastaler@naver.com>
2025-07-16 16:42:29 +0900

    fix(ShiftDownAction): 가상메모리, 현재 위치 정책과 연계해 수정

--- ShiftDownAction.cpp
+++ ShiftDownAction.cpp
@@ -3,6 +3,7 @@
 #include "NotepadForm.h"
 #include "Glyph.h"
 #include "SizeCalculator.h"
+#include "PagingBuffer.h"
 
 #pragma warning(disable:4996)
 
@@ -16,80 +17,145 @@ ShiftDownAction::~ShiftDownAction() {
 }
 
 void ShiftDownAction::Perform() {
+	//1. 현재 위치를 읽는다.
 	Glyph* note = ((NotepadForm*)(this->parent))->note;
 	Long rowIndex = note->GetCurrent();
-	if (rowIndex < note->GetLength() - 1)
-	{
-		Glyph* row = note->GetAt(rowIndex);
-		Long columnIndex = row->GetCurrent();
+	Glyph* row = note->GetAt(rowIndex);
+	Long columnIndex = row->GetCurrent();
 
-		Long originalWidth = 0;
+	//2. 현재 위치가 마지막 페이지의 마지막줄이 아니라면,
+	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
+	if (pagingBuffer->GetEndOffset() < pagingBuffer->GetFileEnd() && rowIndex < note->GetLength() - 1)
+	{
+		//2.1. 현재 위치까지의 너비를 구한다.
+		SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
 		Glyph* character;
-		TCHAR(*content);
+		Long originalWidth = 0;
 		Long i = 0;
 		while (i < columnIndex)
 		{
 			character = row->GetAt(i);
-			content = (char*)(*character);
-			originalWidth += ((NotepadForm*)(this->parent))->sizeCalculator->GetCharacterWidth(
-				const_cast<char*>(content));
+			originalWidth += sizeCalculator->GetCharacterWidth((char*)(*character));
 			i++;
 		}
 
-		Long nextRowIndex = note->Next();
-		Glyph* nextRow = note->GetAt(nextRowIndex);
-
-		Long width = 0;
-		i = 0;
-		while ((i < nextRow->GetLength())&&(width < originalWidth))
-		{
-			character = nextRow->GetAt(i);
-			content = (char*)(*character);
-			width += ((NotepadForm*)(this->parent))->sizeCalculator->GetCharacterWidth(
-				const_cast<char*>(content));
-			i++;
-		}
-
-		Long nextColumnIndex;
-		if (i < nextRow->GetLength())
-		{
-			nextColumnIndex = nextRow->Move(i);
-		}
-		else
-		{
-			nextColumnIndex = nextRow->Last();
-		}
-
+		//2.2. 현재 위치가 줄의 끝이 아니라면,
 		i = columnIndex;
 		while (i < row->GetLength())
 		{
-			character = row->GetAt(i);
-			if (character->IsSelected())
+			//2.2.1. 문자를 읽는다.
+			character = row->GetAt(columnIndex);
+
+			//2.2.2. 문자가 선택되어있지 않다면,
+			if (!character->IsSelected())
 			{
-				character->Select(FALSE);
-			}
-			else
-			{
+				//2.2.2.1. 문자를 선택한다.
 				character->Select(TRUE);
+
+				//2.2.2.2. 페이징버퍼에서 선택시작위치가 표시되어있지 않으면, 표시한다.
+				if (pagingBuffer->GetSelectionBeginOffset() < 0)
+				{
+					pagingBuffer->MarkSelectionBegin();
+				}
+
+				//2.2.2.3. 줄에서 다음 칸으로 이동한다.
+				columnIndex = row->Next();
+				pagingBuffer->Next();
+			}
+			else //2.2.3. 문자가 선택되어 있다면,
+			{
+				//2.2.3.1. 문자를 선택취소한다.
+				character->Select(FALSE);
+
+				//2.2.3.2. 줄에서 다음 칸으로 이동한다.
+				columnIndex = row->Next();
+				pagingBuffer->Next();
+
+				//2.2.3.3. 페이징버퍼에서 현재위치가 선택시작위치와 같으면, 선택시작위치의 표시를 지운다.
+				if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
+				{
+					pagingBuffer->UnmarkSelectionBegin();
+				}
 			}
 			i++;
 		}
 
-		i = 0;
-		while (i < nextColumnIndex)
+		//2.3. 노트에서 다음 줄로 이동한다.
+		rowIndex = note->Next();
+		pagingBuffer->NextRow();
+		if (!pagingBuffer->IsAboveBottomLine())
 		{
-			character = nextRow->GetAt(i);
-			if (character->IsSelected())
-			{
-				character->Select(FALSE);
-			}
-			else
-			{
-				character->Select(TRUE);
-			}
+			pagingBuffer->Load();
+			note = ((NotepadForm*)(this->parent))->note;
+			rowIndex = note->GetCurrent();
+		}
+
+		//2.4. 줄에서 첫번째 칸으로 이동한다.
+		row = note->GetAt(rowIndex);
+		columnIndex = row->First();
+		pagingBuffer->First();
+
+		//2.5. 기존 너비와 가장 가까운 위치를 찾는다.
+		Long previousWidth = 0;
+		Long width = 0;
+		i = 0;
+		while (i < row->GetLength() && width < originalWidth)
+		{
+			character = row->GetAt(i);
+			previousWidth = width;
+			width += sizeCalculator->GetCharacterWidth((char*)(*character));
 			i++;
 		}
 
+		Long index = i;
+		if (width - originalWidth > originalWidth - previousWidth)
+		{
+			index--;
+		}
+
+		//2.6. 찾은 위치까지 반복한다.
+		i = 0;
+		while (i < index)
+		{
+			//2.6.1. 문자를 읽는다.
+			character = row->GetAt(i);
+
+			//2.6.2. 문자가 선택되어 있지 않다면,
+			if (!character->IsSelected())
+			{
+				//2.6.2.1. 문자를 선택한다.
+				character->Select(TRUE);
+
+				//2.6.2.2. 페이징 버퍼에서 선택시작위치가 표시되어 있지 않다면, 표시한다.
+				if (pagingBuffer->GetSelectionBeginOffset() < 0)
+				{
+					pagingBuffer->MarkSelectionBegin();
+				}
+
+				//2.6.2.3. 다음칸으로 이동한다.
+				row->Next();
+				pagingBuffer->Next();
+			}
+			else //2.6.3. 문자가 선택되어 있다면,
+			{
+				//2.6.3.1. 문자를 선택취소한다.
+				character->Select(FALSE);
+
+				//2.6.3.2. 다음 칸으로 이동한다.
+				row->Next();
+				pagingBuffer->Next();
+
+				//2.6.3.3. 페이징버퍼에서 현재위치랑 선택시작위치가 같으면, 표시를 지운다.
+				if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
+				{
+					pagingBuffer->UnmarkSelectionBegin();
+				}
+			}
+
+			i++;
+		}
+
+		((NotepadForm*)(this->parent))->Notify("AdjustScrollBars");
 		this->parent->Invalidate();
 	}
 }
