1b95cbe242a064e5da6aff110b5f4730464d127f
Tale <nastaler@naver.com>
2025-07-24 11:14:22 +0900

    refactor(CtrlShiftAction): 가상메모리 및 현재 위치 정책에 연계해 수정

--- CtrlShiftHomeAction.cpp
+++ CtrlShiftHomeAction.cpp
@@ -2,6 +2,7 @@
 #include "CtrlShiftHomeAction.h"
 #include "NotepadForm.h"
 #include "Glyph.h"
+#include "PagingBuffer.h"
 
 #pragma warning(disable:4966)
 
@@ -15,52 +16,112 @@ CtrlShiftHomeAction::~CtrlShiftHomeAction() {
 }
 
 void CtrlShiftHomeAction::Perform() {
+	//1. 노트에서 현재 위치를 읽는다.
 	Glyph* note = ((NotepadForm*)(this->parent))->note;
 	Long rowIndex = note->GetCurrent();
-	Glyph* originalRow = note->GetAt(rowIndex);
-	Long columnIndex = originalRow->GetCurrent();
+	Glyph* row = note->GetAt(rowIndex);
+	Long columnIndex = row->GetCurrent();
 
-	Long firstRowIndex = note->First();
-	Glyph* row = note->GetAt(firstRowIndex);
-	row->First();
-
+	//2. 줄에서 현재 칸부터 시작 칸까지 반복한다.
+	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
 	Glyph* character;
-	Long j;
-	Long i = 0;
-	while (i < rowIndex)
+	Long i = columnIndex - 1;
+	while (i >= 0)
 	{
-		row = note->GetAt(i);
-		j = 0;
-		while (j < row->GetLength())
+		//2.1. 문자의 선택여부에 따라 선택한다.
+		character = row->GetAt(i);
+		if (!character->IsSelected())
 		{
-			character = row->GetAt(j);
-			if (character->IsSelected())
+			character->Select(TRUE);
+			if (pagingBuffer->GetSelectionBeginOffset() < 0)
 			{
-				character->Select(FALSE);
+				pagingBuffer->MarkSelectionBegin();
 			}
-			else
-			{
-				character->Select(TRUE);
-			}
-			j++;
-		}
-		i++;
-	}
-
-	i = 0;
-	while (i < columnIndex)
-	{
-		character = originalRow->GetAt(i);
-		if (character->IsSelected())
-		{
-			character->Select(FALSE);
+			row->Previous();
+			pagingBuffer->Previous();
 		}
 		else
 		{
-			character->Select(TRUE);
+			character->Select(FALSE);
+			row->Previous();
+			pagingBuffer->Previous();
+			if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
+			{
+				pagingBuffer->UnmarkSelectionBegin();
+			}
 		}
-		i++;
+		i--;
 	}
 
+	if (pagingBuffer->GetSelectionBeginOffset() < 0)
+	{
+		pagingBuffer->MarkSelectionBegin();
+	}
+	rowIndex = note->Previous();
+	pagingBuffer->PreviousRow();
+	if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
+	{
+		pagingBuffer->UnmarkSelectionBegin();
+	}
+
+	//3. 노트에서 처음 위치까지 반복한다.
+	Long j;
+	i = rowIndex;
+	while (i >= 0)
+	{
+		//3.1. 문자의 선택여부에 따라 선택한다.
+		row = note->GetAt(i);
+		j = row->GetLength() - 1;
+		while (j >= 0)
+		{
+			character = row->GetAt(j);
+			if (!character->IsSelected())
+			{
+				character->Select(TRUE);
+				if (pagingBuffer->GetSelectionBeginOffset() < 0)
+				{
+					pagingBuffer->MarkSelectionBegin();
+				}
+				row->Previous();
+				pagingBuffer->Previous();
+			}
+			else
+			{
+				character->Select(FALSE);
+				row->Previous();
+				pagingBuffer->Previous();
+				if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
+				{
+					pagingBuffer->UnmarkSelectionBegin();
+				}
+			}
+			j--;
+		}
+
+		if (pagingBuffer->GetSelectionBeginOffset() < 0)
+		{
+			pagingBuffer->MarkSelectionBegin();
+		}
+		note->Previous();
+		pagingBuffer->Previous();
+		if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
+		{
+			pagingBuffer->UnmarkSelectionBegin();
+		}
+
+		i--;
+	}
+
+	//4. 첫 페이지가 아니면, 첫 페이지를 적재한다.
+	if (pagingBuffer->GetStartOffset() > 0)
+	{
+		pagingBuffer->FirstRow();
+		pagingBuffer->Load();
+	}
+
+	//5. 스크롤바를 조정한다.
+	((NotepadForm*)(this->parent))->Notify("AdjustScrollBars");
+
+	//6. 클라이언트 영역을 갱신한다.
 	this->parent->Invalidate();
 }
