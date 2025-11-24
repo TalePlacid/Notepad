2c2d42eb2de8a3866fad66590442b16a6badf4b4
Tale <nastaler@naver.com>
2025-11-24 12:46:58 +0900

    fix(BackspaceAction): 노트의 첫 위치에서 지우기명령 메시지를 보내던 오류 수정

--- BackspaceAction.cpp
+++ BackspaceAction.cpp
@@ -3,6 +3,7 @@
 #include "BackspaceAction.h"
 #include "NotepadForm.h"
 #include "PagingBuffer.h"
+#include "Glyph.h"
 
 #pragma warning(disable:4996)
 
@@ -21,7 +22,15 @@ void BackspaceAction::Perform() {
 		PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
 		if (pagingBuffer->GetSelectionBeginOffset() < 0)
 		{
-			SendMessage(this->parent->GetSafeHwnd(), WM_COMMAND, (WPARAM)ID_COMMAND_ERASE, 0);
+			Glyph* note = ((NotepadForm*)(this->parent))->note;
+			Long rowIndex = note->GetCurrent();
+			Glyph* row = note->GetAt(rowIndex);
+			Long columnIndex = row->GetCurrent();
+
+			if (rowIndex > 0 || columnIndex > 0)
+			{
+				SendMessage(this->parent->GetSafeHwnd(), WM_COMMAND, (WPARAM)ID_COMMAND_ERASE, 0);
+			}
 		}
 		else
 		{
