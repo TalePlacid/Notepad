6da1981f2f2802b83530c59beb6c4e509d828207
Tale <nastaler@naver.com>
2025-11-24 16:12:09 +0900

    refactor(CtrlRightAction): 적재 정책 변경에 따라 수정

--- CtrlRightAction.cpp
+++ CtrlRightAction.cpp
@@ -4,6 +4,7 @@
 #include "Glyph.h"
 #include "PagingBuffer.h"
 #include "MarkingHelper.h"
+#include "resource.h"
 
 #pragma warning(disable:4996)
 
@@ -17,6 +18,60 @@ CtrlRightAction::~CtrlRightAction() {
 }
 
 void CtrlRightAction::Perform() {
+	Glyph* note = ((NotepadForm*)(this->parent))->note;
+	Long rowIndex = note->GetCurrent();
+	Glyph* row = note->GetAt(rowIndex);
+	Long columnIndex = row->GetCurrent();
+
+	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
+	if (columnIndex < row->GetLength())
+	{
+		Long i = columnIndex;
+		Glyph* character = row->GetAt(i);
+		BOOL isWordCharacter = character->IsWordCharacter();
+		BOOL inBoundary = !isWordCharacter;
+		while (i < row->GetLength() && (!inBoundary || !isWordCharacter))
+		{
+			character = row->GetAt(i);
+			isWordCharacter = character->IsWordCharacter();
+			if (!isWordCharacter)
+			{
+				inBoundary = TRUE;
+			}
+			row->Next();
+			pagingBuffer->Next();
+			i++;
+		}
+
+		if (i < row->GetLength())
+		{
+			row->Previous();
+			pagingBuffer->Previous();
+		}
+	}
+	else
+	{
+		if (note->IsBelowBottomLine(rowIndex + 1))
+		{
+			SendMessage(this->parent->GetSafeHwnd(), WM_COMMAND, (WPARAM)ID_COMMAND_LOADNEXT, 0);
+			rowIndex = note->GetCurrent();
+		}
+
+		if (rowIndex < note->GetLength() - 1)
+		{
+			rowIndex = note->Next();
+			pagingBuffer->NextRow();
+			row = note->GetAt(rowIndex);
+			row->First();
+			pagingBuffer->First();
+		}
+	}
+
+	note->Select(false);
+	MarkingHelper markingHelper(this->parent);
+	markingHelper.Unmark();
+
+#if 0
 	Glyph* note = ((NotepadForm*)(this->parent))->note;
 	Long rowIndex = note->GetCurrent();
 	Glyph* row = note->GetAt(rowIndex);
@@ -74,7 +129,5 @@ void CtrlRightAction::Perform() {
 	note->Select(false);
 	MarkingHelper markingHelper(this->parent);
 	markingHelper.Unmark();
-
-	((NotepadForm*)(this->parent))->Notify("AdjustScrollBars");
-	this->parent->Invalidate();
+#endif
 }
