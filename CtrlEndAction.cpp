e2f0ff5de5c332e79bf4493285196f655ba9adff
Tale <nastaler@naver.com>
2025-11-25 11:19:09 +0900

    refactor(CtrlEndAction): 적재정책 변경에 따른 수정

--- CtrlEndAction.cpp
+++ CtrlEndAction.cpp
@@ -17,29 +17,38 @@ CtrlEndAction::~CtrlEndAction() {
 }
 
 void CtrlEndAction::Perform() {
-	//1. 페이징 버퍼에서 마지막 위치로 이동한다.
+	//1. 페이징버퍼에서 마지막위치로 이동한다.
 	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
 	pagingBuffer->LastRow();
 	pagingBuffer->Last();
-	//2. 페이징 버퍼의 현재 페이지가 파일의 마지막 페이지가 아니면,
-	if (pagingBuffer->GetEndOffset() < pagingBuffer->GetFileEndOffset())
-	{
-		//1.1. 페이징 버퍼에서 마지막 페이지를 적재한다.
-		pagingBuffer->Load();
-	}
-	else //2. 마지막 페이지이면,
-	{
-		//2.1. 노트에서 마지막 위치로 이동한다.
-		Glyph* note = ((NotepadForm*)(this->parent))->note;
-		Long rowIndex = note->Last();
-		Glyph* row = note->GetAt(rowIndex);
-		row->Last();
-	}
 
-	((NotepadForm*)(this->parent))->note->Select(false);
-	MarkingHelper markingHelper(this->parent);
-	markingHelper.Unmark();
+	//2. 페이징버퍼에서 한 줄을 추가한다.
+	TCHAR character[2];
+	character[0] = '\r';
+	character[1] = '\n';
+	pagingBuffer->Add(character);
 
-	((NotepadForm*)(this->parent))->Notify("AdjustScrollBars");
-	this->parent->Invalidate();
+	//3. 페이징 버퍼에서 이전 부분을 적재한다.
+	Glyph* loadedNote = pagingBuffer->LoadPrevious();
+
+	//4. 페이지버퍼에서 추가했던 줄을 지운다.
+	pagingBuffer->Remove();
+
+	//5. 페이징버퍼에서 캐싱한다.
+	Long fileEndOffset = pagingBuffer->GetFileEndOffset();
+	Long totalRowCount = pagingBuffer->CountRow(fileEndOffset);
+	pagingBuffer->CacheRowStartIndex(totalRowCount - loadedNote->GetLength());
+
+	//6. 노트를 교체한다.
+	if (((NotepadForm*)(this->parent))->note != NULL)
+	{
+		delete ((NotepadForm*)(this->parent))->note;
+	}
+	((NotepadForm*)(this->parent))->note = loadedNote;
+
+	//7. 노트에서 마지막으로 이동한다.
+	Glyph* note = ((NotepadForm*)(this->parent))->note;
+	Long rowIndex = note->Last();
+	Glyph* row = note->GetAt(rowIndex);
+	Long columnIndex = row->Last();
 }
