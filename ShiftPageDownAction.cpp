9752f1671823702936216dc746f49b86343ccf36
Tale <nastaler@naver.com>
2025-12-11 17:57:26 +0900

    fix(ShiftPageDownAction): 선택 로직을 오프셋 기반으로 수정

--- ShiftPageDownAction.cpp
+++ ShiftPageDownAction.cpp
@@ -27,6 +27,223 @@ void ShiftPageDownAction::Perform() {
 	Long posLimit = vScroll.GetMax() - vScroll.GetPage();
 	if (scrollController->HasVScroll() && vScroll.GetPos() < posLimit)
 	{
+		//1.1. 내려갈 만큼의 줄 수를 구한다.
+		SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
+		Long rowHeight = sizeCalculator->GetRowHeight();
+		Long rowCount = vScroll.GetPage() / rowHeight;
+		if (vScroll.GetPage() % rowHeight > 0)
+		{
+			rowCount++;
+		}
+
+		//1.2. 현재 너비를 구한다.
+		Glyph* note = ((NotepadForm*)(this->parent))->note;
+		Long rowIndex = note->GetCurrent();
+		Glyph* row = note->GetAt(rowIndex);
+		Long columnIndex = row->GetCurrent();
+		Long rowWidth = sizeCalculator->GetRowWidth(row, columnIndex);
+
+		//1.3. 줄수만큼, 전체 줄위치보다 작으면 반복한다.
+		PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
+		Long selectionBeginOffset;
+		Long previousOffset = -1;
+		Long currentOffset = pagingBuffer->GetCurrentOffset();
+
+		Long previousRowIndex = -1;
+		Long i = 1;
+		while (i <= rowCount && rowIndex != previousRowIndex)
+		{
+			//1.3.2. 현재줄에서 선택한다.
+			while (previousOffset != currentOffset)
+			{
+				//1.3.2.1. 페이징 버퍼에서 마킹한다.
+				selectionBeginOffset = pagingBuffer->GetSelectionBeginOffset();
+				if (selectionBeginOffset < 0)
+				{
+					pagingBuffer->MarkSelectionBegin();
+				}
+				else if (currentOffset == selectionBeginOffset)
+				{
+					pagingBuffer->UnmarkSelectionBegin();
+				}
+
+				//1.3.2.2. 노트에서 선택반전한다.
+				if (columnIndex < row->GetLength())
+				{
+					row->GetAt(columnIndex)->ToggleSelection();
+				}
+
+				previousOffset = currentOffset;
+				currentOffset = pagingBuffer->Next();
+				if (currentOffset == selectionBeginOffset)
+				{
+					pagingBuffer->UnmarkSelectionBegin();
+				}
+				columnIndex = row->Next();
+			}
+
+			//1.3.3. 다음 줄이 적재범위를 넘어서면 재적재한다.
+			if (note->IsBelowBottomLine(rowIndex + 1))
+			{
+				SendMessage(this->parent->GetSafeHwnd(), WM_COMMAND, (WPARAM)ID_COMMAND_LOADNEXT, 0);
+				rowIndex = note->GetCurrent();
+			}
+			
+			//1.3.4. 다음줄이 있다면,
+			if (rowIndex < note->GetLength() - 1)
+			{
+				//1.3.4.1. 다음줄로 이동한다.
+				previousOffset = currentOffset;
+				currentOffset = pagingBuffer->NextRow();
+				previousRowIndex = rowIndex;
+				rowIndex = note->Next();
+				i++;
+				row = note->GetAt(rowIndex);
+				columnIndex = row->First();
+
+				//1.3.4.2. 너비에 근접한 열까지 반복한다.
+				Long nearestColumnIndex = sizeCalculator->GetNearestColumnIndex(row, rowWidth);
+				Long i = 0;
+				while (previousOffset != currentOffset && i < nearestColumnIndex)
+				{
+					//1.3.4.2.1. 페이징 버퍼에서 마킹한다.
+					selectionBeginOffset = pagingBuffer->GetSelectionBeginOffset();
+					if (selectionBeginOffset < 0)
+					{
+						pagingBuffer->MarkSelectionBegin();
+					}
+					else if (currentOffset == selectionBeginOffset)
+					{
+						pagingBuffer->UnmarkSelectionBegin();
+					}
+
+					//1.3.4.2.2. 노트에서 선택반전한다.
+					if (columnIndex < row->GetLength())
+					{
+						row->GetAt(columnIndex)->ToggleSelection();
+					}
+
+					previousOffset = currentOffset;
+					currentOffset = pagingBuffer->Next();
+					if (currentOffset == selectionBeginOffset)
+					{
+						pagingBuffer->UnmarkSelectionBegin();
+					}
+					columnIndex = row->Next();
+					i++;
+				}
+			}
+		}
+	}
+
+
+#if 0
+	//1. 수직 스크롤바가 존재하고, 마지막 위치가 아니라면,
+	ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
+	Scroll vScroll = scrollController->GetVScroll();
+	Long posLimit = vScroll.GetMax() - vScroll.GetPage();
+	if (scrollController->HasVScroll() && vScroll.GetPos() < posLimit)
+	{
+		//1.1. 내려갈 만큼의 줄 수를 구한다.
+		SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
+		Long rowHeight = sizeCalculator->GetRowHeight();
+		Long rowCount = vScroll.GetPage() / rowHeight;
+		if (vScroll.GetPage() % rowHeight > 0)
+		{
+			rowCount++;
+		}
+
+		//1.2. 줄 수만큼, 마지막줄까지 반복한다.
+		Glyph* note = ((NotepadForm*)(this->parent))->note;
+		Long rowIndex = note->GetCurrent();
+		Glyph* row = note->GetAt(rowIndex);
+		Long columnIndex = row->GetCurrent();
+		Long rowWidth = sizeCalculator->GetRowWidth(row, columnIndex);
+
+		PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
+		Long nearestColumnIndex;
+		Long j;
+		Long previousRowIndex = -1;
+		Long i = 1;
+		while (i <= rowCount && rowIndex != previousRowIndex)
+		{
+			//1.2.1. 다음줄이 적재 범위 밖이면 재적재한다.
+			if (note->IsBelowBottomLine(rowIndex + 1))
+			{
+				SendMessage(this->parent->GetSafeHwnd(), WM_COMMAND, (WPARAM)ID_COMMAND_LOADNEXT, 0);
+				rowIndex = note->GetCurrent();
+			}
+
+			//1.2.2. 현재줄에서 이동, 선택한다.
+			while (columnIndex < row->GetLength())
+			{
+				if (!row->GetAt(columnIndex)->IsSelected())
+				{
+					if (pagingBuffer->GetSelectionBeginOffset() < 0)
+					{
+						pagingBuffer->MarkSelectionBegin();
+					}
+					row->GetAt(columnIndex)->Select(true);
+				}
+				else
+				{
+					row->GetAt(columnIndex)->Select(false);
+				} 
+
+				columnIndex = row->Next();
+				pagingBuffer->Next();
+				if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
+				{
+					pagingBuffer->UnmarkSelectionBegin();
+				}
+			}
+
+			//1.2.3. 다음줄로 이동한다.
+			previousRowIndex = rowIndex;
+			rowIndex = note->Next();
+			row = note->GetAt(rowIndex);
+			columnIndex = row->First();
+			pagingBuffer->NextRow();
+			if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
+			{          
+				pagingBuffer->UnmarkSelectionBegin();
+			}
+			i++;
+
+			//1.2.4. 다음줄에서 이동, 선택한다.
+			nearestColumnIndex = sizeCalculator->GetNearestColumnIndex(row, rowWidth);
+			while (columnIndex < nearestColumnIndex && columnIndex < row->GetLength())
+			{
+				if (!row->GetAt(columnIndex)->IsSelected())
+				{
+					if (pagingBuffer->GetSelectionBeginOffset() < 0)
+					{
+						pagingBuffer->MarkSelectionBegin();
+					}
+					row->GetAt(columnIndex)->Select(true);
+				}
+				else
+				{
+					row->GetAt(columnIndex)->Select(false);
+				}
+
+				columnIndex = row->Next();
+				pagingBuffer->Next();
+				if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
+				{
+					pagingBuffer->UnmarkSelectionBegin();
+				}
+			}
+		}
+	}
+#endif
+#if 0
+	//1. 수직 스크롤바가 존재하고, 마지막 위치가 아니라면,
+	ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
+	Scroll vScroll = scrollController->GetVScroll();
+	Long posLimit = vScroll.GetMax() - vScroll.GetPage();
+	if (scrollController->HasVScroll() && vScroll.GetPos() < posLimit)
+	{
 		//1.1 내려갈 만큼의 줄 수를 구한다.
 		SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
 		Long rowHeight = sizeCalculator->GetRowHeight();
@@ -45,13 +262,17 @@ void ShiftPageDownAction::Perform() {
 		Long originalRowWidth = sizeCalculator->GetRowWidth(row, columnIndex);
 
 		//1.3. 노트에서 뒷부분을 선택한다.
+		PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
 		MarkingHelper markingHelper(this->parent);
 		markingHelper.Mark();
-		row->SelectRange(columnIndex, row->GetLength());
+		bool isSelecting = true;
+		if (pagingBuffer->GetCurrentOffset() < pagingBuffer->GetSelectionBeginOffset())
+		{
+			isSelecting = false;
+		}
+		row->SelectRange(columnIndex, row->GetLength(), isSelecting);
 	
 		//1.4. 노트와 페이징버퍼에서 줄 수 만큼 내려간다.
-		PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
-
 		Long restedRowCount = 0;
 		if (rowIndex + rowCount >= note->GetLength())
 		{
@@ -62,12 +283,12 @@ void ShiftPageDownAction::Perform() {
 		row = note->GetAt(rowIndexToMove);
 		row->First();
 		pagingBuffer->NextRow(rowCount);
-		note->SelectRange(rowIndex, rowIndexToMove - 1);
+		note->SelectRange(rowIndex, rowIndexToMove - 1, isSelecting);
 
 		//1.5. 노트와 페이징버퍼에서 너비만큼 이동하며 선택한다.
 		columnIndex = sizeCalculator->GetNearestColumnIndex(row, originalRowWidth);
 		columnIndex = row->Move(columnIndex);
-		row->SelectRange(0, columnIndex);
+		row->SelectRange(0, columnIndex, isSelecting);
 		pagingBuffer->Next(columnIndex);
 
 		//1.6. 줄수가 충분치 않다면,
@@ -89,12 +310,12 @@ void ShiftPageDownAction::Perform() {
 			pagingBuffer->NextRow(restedRowCount);
 
 			//1.6.3. 줄들을 선택한다.
-			note->SelectRange(rowIndex, rowIndexToMove - 1);
+			note->SelectRange(rowIndex, rowIndexToMove - 1, isSelecting);
 
 			//1.6.4. 노트와 페이징버퍼에서 너비만큼 이동하며 선택한다.
 			columnIndex = sizeCalculator->GetNearestColumnIndex(row, originalRowWidth);
 			columnIndex = row->Move(columnIndex);
-			row->SelectRange(0, columnIndex);
+			row->SelectRange(0, columnIndex, isSelecting);
 			pagingBuffer->Next(columnIndex);
 		}
 
@@ -112,4 +333,5 @@ void ShiftPageDownAction::Perform() {
 		}
 		scrollController->MoveVScroll(pos);
 	}
+#endif
 }
