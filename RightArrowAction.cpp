51a6a8c6b135a63e869614b2b34af7b038ba096c
Tale <nastaler@naver.com>
2025-12-26 17:56:45 +0900

    fix(RightArrowAction): 논리 오류 수정

--- RightArrowAction.cpp
+++ RightArrowAction.cpp
@@ -20,6 +20,41 @@ RightArrowAction::~RightArrowAction() {
 }
 
 void RightArrowAction::Perform() {
+	Glyph* note = ((NotepadForm*)(this->parent))->note;
+	Long rowIndex = note->GetCurrent();
+	Glyph* row = note->GetAt(rowIndex);
+	Long columnIndex = row->GetCurrent();
+
+	//1. 줄의 끝이 아니면,
+	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
+	if (columnIndex < row->GetLength())
+	{
+		columnIndex = row->Next();
+		pagingBuffer->Next();
+	}
+	else
+	{
+		SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
+		ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
+		Long pageMax = (pagingBuffer->GetRowStartIndex() + note->GetLength()) * sizeCalculator->GetRowHeight();
+		if (note->IsBelowBottomLine(rowIndex + 1) && pageMax < scrollController->GetVScroll().GetMax())
+		{
+			SendMessage(this->parent->GetSafeHwnd(), WM_COMMAND, (WPARAM)ID_COMMAND_LOADNEXT, 0);
+		}
+
+		if (rowIndex < note->GetLength() - 1)
+		{
+			rowIndex = note->Next();
+			Glyph* nextRow = note->GetAt(rowIndex);
+			columnIndex = nextRow->First();
+
+			if (!nextRow->IsDummyRow())
+			{
+				pagingBuffer->NextRow();
+			}
+		}
+	}
+#if 0	
 	//1. 페이징 버퍼에서 이동한다.
 	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
 	Long previousOffset = pagingBuffer->GetCurrentOffset();
@@ -59,4 +94,5 @@ void RightArrowAction::Perform() {
 			}
 		}
 	}
+#endif
 }
