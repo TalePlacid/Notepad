31d7a33b2b4e4d7722caa3be0bf854f2bf2205d0
Tale <nastaler@naver.com>
2025-12-30 10:22:32 +0900

    refactor(HomeAction): 자동개행을 상정해 수정

--- HomeAction.cpp
+++ HomeAction.cpp
@@ -17,10 +17,16 @@ HomeAction::~HomeAction() {
 }
 
 void HomeAction::Perform() {
+	//1. 현재 위치를 읽는다.
 	Glyph* note = ((NotepadForm*)(this->parent))->note;
 	Long rowIndex = note->GetCurrent();
 	Glyph* row = note->GetAt(rowIndex);
-	row->First();
+	Long columnIndex = row->GetCurrent();
+
+	//2. 노트에서 이동한다.
+	Long firstColumnIndex = row->First();
+
+	//3. 페이징 버퍼에서 이동한다.
 	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
-	pagingBuffer->First();
+	pagingBuffer->Previous(columnIndex - firstColumnIndex);
 }
