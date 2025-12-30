f88bc218c858f2e273697b04484834fc9cd15a22
Tale <nastaler@naver.com>
2025-12-30 11:08:22 +0900

    refactor(CtrlRightAction): 자동개행을 상정해 수정

--- CtrlRightAction.cpp
+++ CtrlRightAction.cpp
@@ -4,6 +4,8 @@
 #include "Glyph.h"
 #include "PagingBuffer.h"
 #include "MarkingHelper.h"
+#include "ScrollController.h"
+#include "SizeCalculator.h"
 #include "resource.h"
 
 #pragma warning(disable:4996)
@@ -18,52 +20,47 @@ CtrlRightAction::~CtrlRightAction() {
 }
 
 void CtrlRightAction::Perform() {
+	//1. 현재 위치를 읽는다.
 	Glyph* note = ((NotepadForm*)(this->parent))->note;
 	Long rowIndex = note->GetCurrent();
 	Glyph* row = note->GetAt(rowIndex);
 	Long columnIndex = row->GetCurrent();
 
+	//2. 줄의 마지막이 아니면,
 	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
 	if (columnIndex < row->GetLength())
 	{
-		Long i = columnIndex;
-		Glyph* character = row->GetAt(i);
-		BOOL isWordCharacter = character->IsWordCharacter();
-		BOOL inBoundary = !isWordCharacter;
-		while (i < row->GetLength() && (!inBoundary || !isWordCharacter))
-		{
-			character = row->GetAt(i);
-			isWordCharacter = character->IsWordCharacter();
-			if (!isWordCharacter)
-			{
-				inBoundary = TRUE;
-			}
-			row->Next();
-			pagingBuffer->Next();
-			i++;
-		}
+		//2.1. 노트에서 다음 단어 시작으로 이동한다.
+		Long wordStart = row->FindNextWordStart(columnIndex);
+		Long movedIndex = row->Move(wordStart);
 
-		if (i < row->GetLength())
-		{
-			row->Previous();
-			pagingBuffer->Previous();
-		}
+		//2.2. 페이징 버퍼에서 이동한다.
+		pagingBuffer->Next(movedIndex - columnIndex);
 	}
-	else
+	else //3. 줄의 마지막이면,
 	{
-		if (note->IsBelowBottomLine(rowIndex + 1))
+		//3.1. 적재범위를 벗어나면, 재적재한다.
+		ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
+		SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
+		Long pageMax = (pagingBuffer->GetRowStartIndex() + note->GetLength()) * sizeCalculator->GetRowHeight();
+		if (note->IsBelowBottomLine(rowIndex + 1) && pageMax < scrollController->GetVScroll().GetMax())
 		{
 			SendMessage(this->parent->GetSafeHwnd(), WM_COMMAND, (WPARAM)ID_COMMAND_LOADNEXT, 0);
-			rowIndex = note->GetCurrent();
 		}
 
-		if (rowIndex < note->GetLength() - 1)
+		//3.2. 마지막 줄이 아니면,
+		if (rowIndex + 1 < note->GetLength())
 		{
+			//3.2.1. 노트에서 이동한다.
 			rowIndex = note->Next();
-			pagingBuffer->NextRow();
-			row = note->GetAt(rowIndex);
-			row->First();
-			pagingBuffer->First();
+			Glyph* nextRow = note->GetAt(rowIndex);
+			columnIndex = nextRow->First();
+
+			//3.2.2. 이동한 줄이 진짜 줄이면, 페이징버퍼에서 이동한다.
+			if (!nextRow->IsDummyRow())
+			{
+				pagingBuffer->NextRow();
+			}
 		}
 	}
 }
