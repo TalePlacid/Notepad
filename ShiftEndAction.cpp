e0dab354c421d6763291be4f7aa6a47eb1381f95
Tale <nastaler@naver.com>
2026-01-02 17:42:47 +0900

    refactor(ShiftEndAction): 메서드를 활용해 간략화

--- ShiftEndAction.cpp
+++ ShiftEndAction.cpp
@@ -16,37 +16,21 @@ ShiftEndAction::~ShiftEndAction() {
 }
 
 void ShiftEndAction::Perform() {
+	//1. 현재 위치를 읽는다.
 	Glyph* note = ((NotepadForm*)(this->parent))->note;
 	Long rowIndex = note->GetCurrent();
 	Glyph* row = note->GetAt(rowIndex);
 	Long columnIndex = row->GetCurrent();
 
+	//2. 줄의 끝까지 반복한다.
 	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
-	Glyph* character;
-	Long i = columnIndex;
-	while (i < row->GetLength())
+	while (columnIndex < row->GetLength())
 	{
-		character = row->GetAt(i);
-		if (!character->IsSelected())
-		{
-			character->Select(TRUE);
-			if (pagingBuffer->GetSelectionBeginOffset() < 0)
-			{
-				pagingBuffer->MarkSelectionBegin();
-			}
-			row->Next();
-			pagingBuffer->Next();
-		}
-		else
-		{
-			character->Select(FALSE);
-			row->Next();
-			pagingBuffer->Next();
-			if (pagingBuffer->GetCurrentOffset() == pagingBuffer->GetSelectionBeginOffset())
-			{
-				pagingBuffer->UnmarkSelectionBegin();
-			}
-		}
-		i++;
+		row->GetAt(columnIndex)->ToggleSelection();
+		columnIndex = row->Next();
+
+		pagingBuffer->BeginSelectionIfNeeded();
+		pagingBuffer->Next();
+		pagingBuffer->EndSelectionIfCollapsed();
 	}
 }
