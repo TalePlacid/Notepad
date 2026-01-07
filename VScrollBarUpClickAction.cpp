7acb52d5897df4f8b023c9f5cf8192f9b5d51904
Tale <nastaler@naver.com>
2026-01-07 12:22:57 +0900

    refactor(VScrollBarUpClickAction): 자동개행을 상정해 수정

--- VScrollBarUpClickAction.cpp
+++ VScrollBarUpClickAction.cpp
@@ -5,6 +5,7 @@
 #include "ScrollController.h"
 #include "PagingBuffer.h"
 #include "Glyph.h"
+#include "UpArrowAction.h"
 #include "resource.h"
 
 #pragma warning(disable:4996)
@@ -19,9 +20,9 @@ VScrollBarUpClickAction::~VScrollBarUpClickAction() {
 }
 
 void VScrollBarUpClickAction::Perform() {
+	//1. 스크롤을 한 줄 올린다.
 	ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
 	SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
-
 	Long rowHeight = sizeCalculator->GetRowHeight();
 	Scroll vScroll = scrollController->GetVScroll();
 	Long pos = vScroll.GetPos() - rowHeight;
@@ -29,28 +30,22 @@ void VScrollBarUpClickAction::Perform() {
 	{
 		pos = 0;
 	}
-	scrollController->MoveVScroll(pos);
+	pos = scrollController->MoveVScroll(pos);
 
-	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
-	Long rowIndexToMove = pos / rowHeight - pagingBuffer->GetRowStartIndex();
-
+	//2. 현재 위치를 읽는다.
 	Glyph* note = ((NotepadForm*)(this->parent))->note;
 	Long rowIndex = note->GetCurrent();
 	Glyph* row = note->GetAt(rowIndex);
 	Long columnIndex = row->GetCurrent();
-	Long rowWidth = sizeCalculator->GetRowWidth(row, columnIndex);
 
-	if (note->IsAboveTopLine(rowIndexToMove) && pagingBuffer->GetRowStartIndex() > 0)
+	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
+	Long rowStartIndex = pagingBuffer->GetRowStartIndex();
+	Long currentPos = (rowStartIndex + rowIndex) * rowHeight;
+
+	//3. 보이는 영역을 벗어났다면, 한 줄 올린다.
+	if (currentPos + rowHeight > pos + vScroll.GetPage())
 	{
-		SendMessage(this->parent->GetSafeHwnd(), WM_COMMAND, (WPARAM)ID_COMMAND_LOADPREVIOUS, 0);
-		rowIndex = note->GetCurrent();
-		rowIndexToMove = pos / rowHeight - pagingBuffer->GetRowStartIndex();
+		UpArrowAction upArrowAction(this->parent);
+		upArrowAction.Perform();
 	}
-
-	pagingBuffer->PreviousRow(rowIndex - rowIndexToMove);
-	rowIndex = note->Move(rowIndexToMove);
-	row = note->GetAt(rowIndex);
-	columnIndex = sizeCalculator->GetNearestColumnIndex(row, rowWidth);
-	pagingBuffer->Next(columnIndex);
-	columnIndex = row->Move(columnIndex);
 }
