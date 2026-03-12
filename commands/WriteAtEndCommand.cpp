c253fc87bfd395805106661231221f6f10e0cf7c
Tale <nastaler@naver.com>
2026-03-12 17:55:23 +0900

    fix(WriteAtEndCommand): 범위 선택 후 한글 조합 문자 입력시 오동작하던 오류 수정

--- commands/WriteAtEndCommand.cpp
+++ commands/WriteAtEndCommand.cpp
@@ -62,24 +62,18 @@ WriteAtEndCommand& WriteAtEndCommand::operator=(const WriteAtEndCommand& source)
 }
 
 void WriteAtEndCommand::Execute() {
-	this->isErased = FALSE;
-	this->erasedFrontOffset = -1;
-	this->erasedRearOffset = -1;
-	this->erasedColumnIndex = 0;
-	this->erasedContents = "";
-
-	if (this->onChar)
+	//1. 선택범위가 있으면, 먼저 지운다.
+	Editor editor(this->parent);
+	this->isErased = editor.GetSelectedRange(this->erasedFrontOffset, this->erasedRearOffset);
+	if (this->isErased == TRUE)
 	{
-		Editor editor(this->parent);
-		this->isErased = editor.GetSelectedRange(this->erasedFrontOffset, this->erasedRearOffset);
-		if (this->isErased)
-		{
-			PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
-			this->erasedContents = pagingBuffer->MakeSelectedString();
-			editor.EraseRange(this->erasedFrontOffset, this->erasedRearOffset, this->erasedColumnIndex);
-		}
+		this->isUndoable = true;
+		PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
+		this->erasedContents = pagingBuffer->MakeSelectedString();
+		editor.EraseRange(this->erasedFrontOffset, this->erasedRearOffset, this->erasedColumnIndex);
 	}
 
+	//2. 끝 위치에 문자를 반영한다.
 	GlyphFactory glyphFactory;
 	Glyph* glyph = glyphFactory.Create(this->character);
 	
@@ -146,58 +140,62 @@ void WriteAtEndCommand::Undo() {
 	caretNavigator.MoveTo(this->offset);
 	caretNavigator.NormalizeColumn(this->columnIndex);
 
-	Glyph* note = ((NotepadForm*)(this->parent))->note;
-	Long rowIndex = note->GetCurrent();
-	Glyph* row = note->GetAt(rowIndex);
-	this->columnIndex = row->GetCurrent();
-
-	//2. 지운다.
-	ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
-	SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
-	if (this->columnIndex > 0)
+	//2. 확정문자였었으면, 지운다.
+	if (this->onChar)
 	{
-		row->Remove();
+		Glyph* note = ((NotepadForm*)(this->parent))->note;
+		Long rowIndex = note->GetCurrent();
+		Glyph* row = note->GetAt(rowIndex);
+		this->columnIndex = row->GetCurrent();
 
-		if (((NotepadForm*)(this->parent))->IsAutoWrapped())
+		ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
+		SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
+		if (this->columnIndex > 0)
 		{
-			NoteWrapper noteWrapper(this->parent);
-			Long dummied = noteWrapper.Rewrap();
+			row->Remove();
+
+			if (((NotepadForm*)(this->parent))->IsAutoWrapped())
+			{
+				NoteWrapper noteWrapper(this->parent);
+				Long dummied = noteWrapper.Rewrap();
+				rowIndex = note->GetCurrent();
+				row = note->GetAt(rowIndex);
+
+				if (scrollController->HasVScroll())
+				{
+					Scroll vScroll = scrollController->GetVScroll();
+					Long max = vScroll.GetMax() + dummied * sizeCalculator->GetRowHeight();
+					scrollController->ResizeVRange(max);
+				}
+			}
+			this->columnIndex = row->GetCurrent();
+		}
+		else
+		{
+			if (note->IsAboveTopLine(rowIndex - 1))
+			{
+				PageLoader::LoadPrevious(this->parent);
+			}
+			note->Remove();
 			rowIndex = note->GetCurrent();
 			row = note->GetAt(rowIndex);
+			this->columnIndex = row->Last();
 
 			if (scrollController->HasVScroll())
 			{
 				Scroll vScroll = scrollController->GetVScroll();
-				Long max = vScroll.GetMax() + dummied * sizeCalculator->GetRowHeight();
+				Long max = vScroll.GetMax() - sizeCalculator->GetRowHeight();
 				scrollController->ResizeVRange(max);
 			}
 		}
-		this->columnIndex = row->GetCurrent();
-	}
-	else
-	{
-		if (note->IsAboveTopLine(rowIndex - 1))
-		{
-			PageLoader::LoadPrevious(this->parent);
-		}
-		note->Remove();
-		rowIndex = note->GetCurrent();
-		row = note->GetAt(rowIndex);
-		this->columnIndex = row->Last();
 
-		if (scrollController->HasVScroll())
-		{
-			Scroll vScroll = scrollController->GetVScroll();
-			Long max = vScroll.GetMax() - sizeCalculator->GetRowHeight();
-			scrollController->ResizeVRange(max);
-		}
+		PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
+		pagingBuffer->Remove();
+
+		this->offset = pagingBuffer->GetCurrentOffset();
 	}
 
-	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
-	pagingBuffer->Remove();
-
-	this->offset = pagingBuffer->GetCurrentOffset();
-
+	//3. 범위가 지워졌었으면, 다시 적는다.
 	if (this->isErased)
 	{
 		Editor editor(this->parent);
@@ -206,74 +204,78 @@ void WriteAtEndCommand::Undo() {
 }
 
 void WriteAtEndCommand::Redo() {
+	//1. 선택범위가 지워졌었으면, 다시 지운다.
 	if (this->isErased)
 	{
 		Editor editor(this->parent);
 		editor.EraseRange(this->erasedFrontOffset, this->erasedRearOffset, this->erasedColumnIndex);
 	}
 
-	//1. 원래 위치로 이동한다.
-	GlyphFactory glyphFactory;
-	Glyph* glyph = glyphFactory.Create(this->character);
-
-	CaretNavigator caretNavigator(parent);
-	caretNavigator.MoveTo(this->offset);
-	caretNavigator.NormalizeColumn(this->columnIndex);
-
-	//2. 다시 쓴다.
-	Glyph* note = ((NotepadForm*)(this->parent))->note;
-	Long rowIndex = note->GetCurrent();
-	Glyph* row = note->GetAt(rowIndex);
-	this->columnIndex = row->GetCurrent();
-
-	PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
-	ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
-	SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
-	if (this->character[0] != '\r')
+	//2. 확정문자였었으면, 다시 쓴다.
+	if (this->onChar)
 	{
-		row->Add(glyph);
-		pagingBuffer->Add((char*)(*glyph));
-	
-		if (((NotepadForm*)(this->parent))->IsAutoWrapped())
+		//2.1. 원래 위치로 이동한다.
+		GlyphFactory glyphFactory;
+		Glyph* glyph = glyphFactory.Create(this->character);
+
+		CaretNavigator caretNavigator(parent);
+		caretNavigator.MoveTo(this->offset);
+		caretNavigator.NormalizeColumn(this->columnIndex);
+
+		//2.2. 다시 쓴다.
+		Glyph* note = ((NotepadForm*)(this->parent))->note;
+		Long rowIndex = note->GetCurrent();
+		Glyph* row = note->GetAt(rowIndex);
+		this->columnIndex = row->GetCurrent();
+
+		PagingBuffer* pagingBuffer = ((NotepadForm*)(this->parent))->pagingBuffer;
+		ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
+		SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
+		if (this->character[0] != '\r')
 		{
-			NoteWrapper noteWrapper(this->parent);
-			Long dummied = noteWrapper.Rewrap();
+			row->Add(glyph);
+			pagingBuffer->Add((char*)(*glyph));
+		
+			if (((NotepadForm*)(this->parent))->IsAutoWrapped())
+			{
+				NoteWrapper noteWrapper(this->parent);
+				Long dummied = noteWrapper.Rewrap();
+				rowIndex = note->GetCurrent();
+				row = note->GetAt(rowIndex);
+
+				if (scrollController->HasVScroll())
+				{
+					Scroll vScroll = scrollController->GetVScroll();
+					Long max = vScroll.GetMax() + dummied * sizeCalculator->GetRowHeight();
+					scrollController->ResizeVRange(max);
+				}
+			}
+			this->columnIndex = row->GetCurrent();
+		}
+		else
+		{
+			note->Add(glyph);
 			rowIndex = note->GetCurrent();
 			row = note->GetAt(rowIndex);
+			this->columnIndex = row->First();
+
+			TCHAR contents[2];
+			contents[0] = '\r';
+			contents[1] = '\n';
+			pagingBuffer->Add(contents);
 
 			if (scrollController->HasVScroll())
 			{
 				Scroll vScroll = scrollController->GetVScroll();
-				Long max = vScroll.GetMax() + dummied * sizeCalculator->GetRowHeight();
+				Long max = vScroll.GetMax() + sizeCalculator->GetRowHeight();
 				scrollController->ResizeVRange(max);
 			}
 		}
-		this->columnIndex = row->GetCurrent();
+		this->offset = pagingBuffer->GetCurrentOffset();
 	}
-	else
-	{
-		note->Add(glyph);
-		rowIndex = note->GetCurrent();
-		row = note->GetAt(rowIndex);
-		this->columnIndex = row->First();
-
-		TCHAR contents[2];
-		contents[0] = '\r';
-		contents[1] = '\n';
-		pagingBuffer->Add(contents);
-
-		ScrollController* scrollController = ((NotepadForm*)(this->parent))->scrollController;
-		SizeCalculator* sizeCalculator = ((NotepadForm*)(this->parent))->sizeCalculator;
-		if (scrollController->HasVScroll())
-		{
-			Scroll vScroll = scrollController->GetVScroll();
-			Long max = vScroll.GetMax() + sizeCalculator->GetRowHeight();
-			scrollController->ResizeVRange(max);
-		}
-	}
-	this->offset = pagingBuffer->GetCurrentOffset();
 }
 
 Command* WriteAtEndCommand::Clone() {
 	return new WriteAtEndCommand(*this);
 }
+
